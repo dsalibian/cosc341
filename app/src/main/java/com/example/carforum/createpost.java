a9d23cffd4cef39b3d5f0c2d0fdce3b251c6f08d
dsalibian <dsalibian@proton.me>
2025-12-03 12:11:38 -0800

    add createpost skeleton

---
+++ app/src/main/java/com/example/carforum/createpost.java
@@ -0,0 +1,71 @@
+package com.example.carforum;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+
+public class createpost extends AppCompatActivity {
+
+    void showmsg(String s) { Toast.makeText(createpost.this, s, Toast.LENGTH_SHORT).show(); }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_createpost);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ArrayList<String> tags = new ArrayList<>();
+        ArrayList<String> comments = new ArrayList<>();
+
+        TextInputEditText input_title = findViewById(R.id.titleinput);
+        TextInputEditText input_tags = findViewById(R.id.tagsinput);
+        TextInputEditText input_content = findViewById(R.id.contentinput);
+
+        Button cancelbtn = findViewById(R.id.cancelbtn);
+        Button postbtn = findViewById(R.id.postbtn);
+
+        postbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title   = input_title.getText().toString().trim();
+                String tags    = input_tags.getText().toString().trim();
+                String content = input_content.getText().toString().trim();
+
+                if (title.isEmpty() || tags.isEmpty() || content.isEmpty()) {
+                    showmsg("empty fields");
+                    return;
+                }
+
+                Post p = new Post(title, "temp", content);
+
+                showmsg("post created");
+
+                // handle post creation and change author field to current user
+                throw new RuntimeException("implement post creation");
+
+                //finish();
+            }
+        });
+
+        cancelbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { finish(); }
+        });
+    }
+}
