3370806ea1e277b041e32f770a7457d8039e944f
dsalibian <dsalibian@proton.me>
2025-12-03 18:16:54 -0800

    global post array

--- app/src/main/java/com/example/carforum/MainActivity.java
+++ app/src/main/java/com/example/carforum/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.carforum;
 
-import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static ArrayList<Post> posts;
+
+    void init_posts() {
+        posts = new ArrayList<>();
+
+        for(int i = 0; i < posts.size(); ++i) {
+            posts.add(new Post("title" + i, "user" + i, "content" + i));
+
+            for(int j = 0; j < 3; ++j) {
+                posts.get(i).addTag("tag" + i);
+                posts.get(i).addComment("comment" + i);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        finish();
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        init_posts();
+
+
     }
 }
